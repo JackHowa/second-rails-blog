7584adc0885eb1b485a7e8d3ef1138982de8c42f
jackhowa <johncarverhoward@gmail.com>
2017-06-18 17:07:17 -0500

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+	# rails generated via $ rails g controller posts 
+
+	def index # connected via the config/routes
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def new # Makes a new form 
+		@post = Post.new
+	end
+
+	def create # Post route for Post
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new' # redirect would lose the flash error message
+		end
+	end 
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end 
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end 
+
+	def destroy 
+		@post = Post.find(params[:id])
+		@post.destroy 
+		redirect_to root_path
+	end
+
+	private 
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end 
+end
---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+	# if post deleted, comments too
+	has_many :comments, dependent: :destroy 
+	
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :body, presence: true
+end
